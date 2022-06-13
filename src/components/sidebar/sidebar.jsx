1b73c43addc7b44b5a6827c6680f68e5542b3df9
Jeongbin <yon980824@gmail.com>
2022-06-13 12:04:22 +0900

    Sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,8 +1,73 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./sidebar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = (props) => {
-  return <div className={styles.sidebar}></div>;
+  const openSubNav = (e) => {
+    const subNav = e.currentTarget.nextSibling;
+    if (subNav.classList.contains(`${styles["active"]}`)) {
+      subNav.classList.remove(`${styles["active"]}`);
+    } else {
+      subNav.classList.add(`${styles["active"]}`);
+    }
+    console.log(subNav);
+  };
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.profile}>
+        <div className={styles.imgBox}>
+          <img src="./user.png" alt="프로필 이미지" />
+        </div>
+        <span className={styles.userName}>아이디</span>
+      </div>
+      <nav>
+        <ul className={styles.nav}>
+          <li>
+            <div onClick={openSubNav}>
+              <span>상품관리</span>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </div>
+            <ul className={styles.nav_sub}>
+              <li>상품조회/수정</li>
+              <li>상품등록</li>
+              <li>배송정보 관리</li>
+            </ul>
+          </li>
+          <li>
+            <div onClick={openSubNav}>
+              <span>판매관리</span>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </div>
+            <ul className={styles.nav_sub}>
+              <li>주문통합검색</li>
+              <li>미결제 확인</li>
+            </ul>
+          </li>
+          <li>
+            <div onClick={openSubNav}>
+              <span>정산관리</span>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </div>
+            <ul className={styles.nav_sub}>
+              <li>서브 카테고리</li>
+              <li>서브 카테고리</li>
+            </ul>
+          </li>
+          <li>
+            <div onClick={openSubNav}>
+              <span>문의/리뷰관리</span>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </div>
+            <ul className={styles.nav_sub}>
+              <li>서브 카테고리</li>
+              <li>서브 카테고리</li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Sidebar;

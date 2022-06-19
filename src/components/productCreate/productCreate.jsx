c1f2a079360709e4822a5a3bfe679c0acf14c79e
Jeongbin <yon980824@gmail.com>
2022-06-19 18:04:34 +0900

    Upload data

--- src/components/productCreate/productCreate.jsx
+++ src/components/productCreate/productCreate.jsx
@@ -2,15 +2,122 @@ import React from "react";
 import styles from "./productCreate.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useRef } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const ProductCreate = (props) => {
+  // 데이터 업로드
+  // const [data, setData] = useState("");
+  const upLoadData = async (data) => {
+    console.log(data);
+    const response = await axios
+      .post("localhost/api/car-save", data)
+      .then((res) => res.data);
+  };
+
+  const [accident, setAccident] = useState(false);
+  const [useChange, setUseChange] = useState(false);
+  const [useChangeHistory, setUseChangeHistory] = useState(false);
+  const [specialAccidentHistory, setSpecialAccidentHistory] = useState(false);
   const categoryBtnClick = (e) => {
-    console.log(e.target);
     const currentTarget = e.currentTarget.childNodes;
     [...currentTarget].map((btn) => {
       btn.classList.remove(`${styles["active"]}`);
     });
     e.target.classList.add(`${styles["active"]}`);
+
+    if (e.currentTarget.classList.contains(`${styles["accident"]}`)) {
+      if (e.target.innerHTML === "있음") setAccident(true);
+      else setAccident(false);
+    }
+    if (e.currentTarget.classList.contains(`${styles["useChange"]}`)) {
+      if (e.target.innerHTML === "있음") setUseChange(true);
+      else setUseChange(false);
+    }
+    if (e.currentTarget.classList.contains(`${styles["useChangeHistory"]}`)) {
+      if (e.target.innerHTML === "있음") setUseChangeHistory(true);
+      else setUseChangeHistory(false);
+    }
+    if (
+      e.currentTarget.classList.contains(`${styles["specialAccidentHistory"]}`)
+    ) {
+      if (e.target.innerHTML === "있음") setSpecialAccidentHistory(true);
+      else setSpecialAccidentHistory(false);
+    }
+  };
+
+  // 저장하기
+  const categoryRef = useRef();
+  const nameRef = useRef();
+  const carPriceRef = useRef();
+  const stockQuantityRef = useRef();
+  const registrationFeeRef = useRef();
+  const managementCostRef = useRef();
+  const vehicleTypeRef = useRef();
+  const seaterRef = useRef();
+  const modelYearRef = useRef();
+  const mileageRef = useRef();
+  const colorRef = useRef();
+  const fuelRef = useRef();
+  const importStatusRef = useRef();
+  const manufacturerRef = useRef();
+  const modelRef = useRef();
+  const detailModelRef = useRef();
+  const salesStatusRef = useRef();
+  const carNumberRef = useRef();
+  const transmissionRef = useRef();
+  const driveTypeRef = useRef();
+  const sheetMetalRef = useRef();
+  const exchangeRef = useRef();
+  const detailedConditionRef = useRef();
+  const damageMyCarRef = useRef();
+  const relativeDamageRef = useRef();
+  const assessorEmployeeNumberRef = useRef();
+
+  const submit = () => {
+    const inputData = {
+      categoryName: categoryRef.current.value,
+      name: nameRef.current.value,
+      purchaseCost: {
+        carPrice: Number(carPriceRef.current.value),
+        registrationFee: Number(registrationFeeRef.current.value),
+        managementCost: Number(managementCostRef.current.value),
+      },
+      stockQuantity: Number(stockQuantityRef.current.value),
+      vehicleType: vehicleTypeRef.current.value,
+      seater: seaterRef.current.value,
+      modelYear: modelYearRef.current.value,
+      mileage: mileageRef.current.value,
+      color: colorRef.current.value,
+      fuel: fuelRef.current.value,
+      importStatus: importStatusRef.current.value,
+      manufacturer: manufacturerRef.current.value,
+      model: modelRef.current.value,
+      detailModel: detailModelRef.current.value,
+      salesStatus: salesStatusRef.current.value,
+      carNumber: carNumberRef.current.value,
+      salesStatus: salesStatusRef.current.value,
+      transmission: transmissionRef.current.value,
+      accident,
+      driveType: driveTypeRef.current.value,
+      inspectionRecord: {
+        sheetMetal: Number(sheetMetalRef.current.value),
+        useChange,
+        exchange: Number(exchangeRef.current.value),
+        detailedCondition: Number(detailedConditionRef.current.value),
+      },
+      insuranceHistory: {
+        damageMyCar: Number(damageMyCarRef.current.value),
+        relativeDamage: Number(relativeDamageRef.current.value),
+        useChangeHistory,
+        specialAccidentHistory,
+      },
+      assessorEmployeeNumber: assessorEmployeeNumberRef.current.value,
+    };
+    // setData(inputData);
+    upLoadData(inputData);
+    // console.log(inputData);
   };
   return (
     <div>
@@ -24,7 +131,7 @@ const ProductCreate = (props) => {
           >
             카레고리명 선택
           </div>
-          <select>
+          <select ref={categoryRef}>
             <option value="">선택</option>
             <option value="차량검색">차량검색</option>
             <option value="리스차량">리스차량</option>
@@ -40,7 +147,7 @@ const ProductCreate = (props) => {
       <div className={styles.container}>
         <div className={styles.container_title}>상품명</div>
         <div className={styles.container_body}>
-          <input type="text" />
+          <input ref={nameRef} type="text" />
         </div>
       </div>
 
@@ -51,19 +158,31 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>차량가격</div>
             <div>
-              <input type="number" placeholder="숫자만 입력" />
+              <input
+                ref={carPriceRef}
+                type="number"
+                placeholder="숫자만 입력"
+              />
             </div>
           </div>
           <div>
             <div className={styles.body_title}>이전등록비</div>
             <div>
-              <input type="number" placeholder="숫자만 입력" />
+              <input
+                ref={registrationFeeRef}
+                type="number"
+                placeholder="숫자만 입력"
+              />
             </div>
           </div>
           <div>
             <div className={styles.body_title}>관리비용</div>
             <div>
-              <input type="number" placeholder="숫자만 입력" />
+              <input
+                ref={managementCostRef}
+                type="number"
+                placeholder="숫자만 입력"
+              />
             </div>
           </div>
         </div>
@@ -73,7 +192,11 @@ const ProductCreate = (props) => {
       <div className={styles.container}>
         <div className={styles.container_title}>재고수량</div>
         <div className={styles.container_body}>
-          <input type="number" placeholder="숫자만 입력" />
+          <input
+            ref={stockQuantityRef}
+            type="number"
+            placeholder="숫자만 입력"
+          />
         </div>
       </div>
 
@@ -84,7 +207,7 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>차종</div>
             <div>
-              <select>
+              <select ref={vehicleTypeRef}>
                 <option value="">차종을 선택해주세요</option>
                 <option value="경차">경차</option>
                 <option value="소형차">소형차</option>
@@ -105,7 +228,7 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>인승</div>
             <div>
-              <select>
+              <select ref={seaterRef}>
                 <option value="">인승을 입력해주세요</option>
                 <option value="4">4인승</option>
                 <option value="5">5인승</option>
@@ -120,21 +243,21 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>연식</div>
             <div>
-              <input type="number" />
+              <input ref={modelYearRef} type="number" />
             </div>
           </div>
 
           <div>
             <div className={styles.body_title}>주행거리</div>
             <div>
-              <input type="number" />
+              <input ref={mileageRef} type="number" />
             </div>
           </div>
 
           <div>
             <div className={styles.body_title}>색상</div>
             <div>
-              <select>
+              <select ref={colorRef}>
                 <option value="">색상을 선택해주세요</option>
                 <option value="흰색">흰색</option>
                 <option value="진주색">진주색</option>
@@ -149,7 +272,7 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>연료</div>
             <div>
-              <select>
+              <select ref={fuelRef}>
                 <option value="">연료를 선택해주세요</option>
                 <option value="가솔린">가솔린</option>
                 <option value="디젤">디젤</option>
@@ -163,9 +286,9 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>수입여부</div>
             <div>
-              <select>
-                <option value="">국산</option>
-                <option value="">수입</option>
+              <select ref={importStatusRef}>
+                <option value="0">국산</option>
+                <option value="1">수입</option>
               </select>
             </div>
           </div>
@@ -173,28 +296,28 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>제조사</div>
             <div>
-              <input type="text" />
+              <input ref={manufacturerRef} type="text" />
             </div>
           </div>
 
           <div>
             <div className={styles.body_title}>모델</div>
             <div>
-              <input type="text" />
+              <input ref={modelRef} type="text" />
             </div>
           </div>
 
           <div>
             <div className={styles.body_title}>세부모델</div>
             <div>
-              <input type="text" />
+              <input ref={detailModelRef} type="text" />
             </div>
           </div>
 
           <div>
             <div className={styles.body_title}>판매상태</div>
             <div>
-              <select>
+              <select ref={salesStatusRef}>
                 <option value="">선택</option>
                 <option value="READY">준비중</option>
                 <option value="ON">판매중</option>
@@ -206,14 +329,14 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>차량번호</div>
             <div>
-              <input type="text" />
+              <input ref={carNumberRef} type="text" />
             </div>
           </div>
 
           <div>
             <div className={styles.body_title}>변속기</div>
             <div>
-              <select>
+              <select ref={transmissionRef}>
                 <option value="">선택</option>
                 <option value="오토">오토</option>
                 <option value="수동">수동</option>
@@ -226,13 +349,12 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>사고유무</div>
             <div>
-              <div onClick={categoryBtnClick} className={styles.category_btns}>
+              <div
+                onClick={categoryBtnClick}
+                className={`${styles.category_btns} ${styles.accident}`}
+              >
                 <div className={styles.catetory_select}>있음</div>
-                <div
-                  className={`${styles["catetory_search"]} ${styles["active"]}`}
-                >
-                  없음
-                </div>
+                <div className={styles.active}>없음</div>
               </div>
             </div>
           </div>
@@ -240,7 +362,7 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>구동방식</div>
             <div>
-              <select>
+              <select ref={driveTypeRef}>
                 <option value="">선택</option>
                 <option value="전륜">전륜</option>
                 <option value="후륜">후륜</option>
@@ -258,7 +380,7 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>대표이미지</div>
             <div>
-              <label className={styles.inputFileButton} for="input-file">
+              <label className={styles.inputFileButton} htmlFor="input-file">
                 +
               </label>
               <input type="file" id="input-file" style={{ display: "none" }} />
@@ -274,32 +396,35 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>판금</div>
             <div>
-              <input type="number" placeholder={"0건"} />
+              <input ref={sheetMetalRef} type="number" placeholder={"0건"} />
             </div>
           </div>
           <div>
             <div className={styles.body_title}>교환</div>
             <div>
-              <input type="number" placeholder={"0건"} />
+              <input ref={exchangeRef} type="number" placeholder={"0건"} />
             </div>
           </div>
           <div>
             <div className={styles.body_title}>용도변경</div>
             <div>
-              <div onClick={categoryBtnClick} className={styles.category_btns}>
+              <div
+                onClick={categoryBtnClick}
+                className={`${styles.category_btns} ${styles.useChange}`}
+              >
                 <div className={styles.catetory_select}>있음</div>
-                <div
-                  className={`${styles["catetory_search"]} ${styles["active"]}`}
-                >
-                  없음
-                </div>
+                <div className={styles.active}>없음</div>
               </div>
             </div>
           </div>
           <div>
             <div className={styles.body_title}>세부상태 확인</div>
             <div>
-              <input type="number" placeholder={"0건"} />
+              <input
+                ref={detailedConditionRef}
+                type="number"
+                placeholder={"0건"}
+              />
             </div>
           </div>
         </div>
@@ -312,38 +437,40 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>내차 피해</div>
             <div>
-              <input type="text" placeholder={"0건"} />
+              <input ref={damageMyCarRef} type="number" placeholder={"0건"} />
             </div>
           </div>
           <div>
             <div className={styles.body_title}>상대차 피해</div>
             <div>
-              <input type="text" placeholder={"0건"} />
+              <input
+                ref={relativeDamageRef}
+                type="number"
+                placeholder={"0건"}
+              />
             </div>
           </div>
           <div>
             <div className={styles.body_title}>용도변경이력</div>
             <div>
-              <div onClick={categoryBtnClick} className={styles.category_btns}>
+              <div
+                onClick={categoryBtnClick}
+                className={`${styles.category_btns} ${styles.useChangeHistory}`}
+              >
                 <div className={styles.catetory_select}>있음</div>
 
-                <div
-                  className={`${styles["catetory_search"]} ${styles["active"]}`}
-                >
-                  없음
-                </div>
+                <div className={styles.active}>없음</div>
               </div>
             </div>
           </div>
           <div>
             <div className={styles.body_title}>특수사고이력</div>
-            <div onClick={categoryBtnClick} className={styles.category_btns}>
+            <div
+              onClick={categoryBtnClick}
+              className={`${styles.category_btns} ${styles.specialAccidentHistory}`}
+            >
               <div className={styles.catetory_select}>있음</div>
-              <div
-                className={`${styles["catetory_search"]} ${styles["active"]}`}
-              >
-                없음
-              </div>
+              <div className={styles.active}>없음</div>
             </div>
           </div>
         </div>
@@ -356,14 +483,20 @@ const ProductCreate = (props) => {
           <div>
             <div className={styles.body_title}>사번</div>
             <div>
-              <input type="text" placeholder={"0건"} />
+              <input
+                ref={assessorEmployeeNumberRef}
+                type="text"
+                placeholder={"0건"}
+              />
             </div>
           </div>
         </div>
       </div>
 
       {/* 등록하기 */}
-      <button className={styles.submitBtn}>등록하기</button>
+      <button onClick={submit} className={styles.submitBtn}>
+        등록하기
+      </button>
     </div>
   );
 };
